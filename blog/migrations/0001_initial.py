2564f3ef85d73bbf7ba5c6f55600cff4321034b7
trojan334 <lukasz.trojanowicz@gmail.com>
2023-04-25 20:11:19 +0200

    fix

--- blog/migrations/0001_initial.py
+++ blog/migrations/0001_initial.py
@@ -23,7 +23,7 @@ class Migration(migrations.Migration):
                 ('title', models.CharField(max_length=200)),
                 ('text', models.TextField()),
                 ('created_date', models.DateTimeField(default=datetime.datetime(2023, 4, 19, 18, 59, 22, 83435, tzinfo=utc))),
-                ('publish_date', models.DateTimeField(blank=True, null=True)),
+                ('published_date', models.DateTimeField(blank=True, null=True)),
                 ('author', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL)),
             ],
         ),
